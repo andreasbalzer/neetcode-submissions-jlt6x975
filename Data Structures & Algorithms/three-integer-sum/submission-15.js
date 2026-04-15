36833ebbef7075f039f026f379fcaea4c79fdc70
Andreas Balzer <github@andreas-balzer.de>
2026-04-15 15:59:14 +0100

    Add: three-integer-sum - submission-15

---
+++ Data Structures & Algorithms/three-integer-sum/submission-15.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        const result = [];
+        nums = nums.sort((a, b) => a - b);
+        for (let index = 0; index < nums.length - 2; index++) {
+            if (index > 0 && nums[index] === nums[index - 1]) {
+                continue;
+            }
+            let left = index + 1;
+            let right = nums.length - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] + nums[index] === 0) {
+                    result.push([nums[index], nums[left], nums[right]]);
+                    left++;
+                    right--;
+                    while (nums[left] === nums[left - 1] && left < right) {
+                        left++;
+                    }
+                }
+                else if (nums[left] + nums[right] + nums[index] < 0) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+}
