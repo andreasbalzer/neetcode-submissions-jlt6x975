6c0bcb4e899f368eba57e89b66d4af885e9d097f
Andreas Balzer <github@andreas-balzer.de>
2026-04-17 20:43:35 +0100

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stack = [];
+        const mapping = {
+            ")": "(",
+            "]": "[",
+            "}": "{"
+        };
+
+        for (let char of s) {
+            if (mapping[char]) {
+                if (stack[stack.length - 1] !== mapping[char]) {
+                    return false;
+                }
+
+                stack.pop();
+            }
+            else {
+                stack.push(char);
+            }
+        }
+
+        return stack.length === 0;
+    }
+}
