35a02de21769500c9aadf0abb254221aa0bd1a22
Andreas Balzer <github@andreas-balzer.de>
2026-04-18 13:52:33 +0100

    Add: car-fleet - submission-19

---
+++ Data Structures & Algorithms/car-fleet/submission-19.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        const carFleets = [];
+        for (let carFleet = 0; carFleet < position.length; carFleet++) {
+            carFleets[carFleet] = {
+                position: position[carFleet],
+                speed: speed[carFleet],
+                arriveAt: (target - position[carFleet]) / speed[carFleet]
+            };
+        }
+        
+        carFleets.sort((a, b) => b.position - a.position);
+
+        let carFleetCount = carFleets.length;
+        const stack = [];
+
+        for (let index = 0; index < carFleets.length; index++) {
+            stack.push(carFleets[index]);
+           if (stack.length >= 2 && stack[stack.length - 2].arriveAt >= stack[stack.length - 1].arriveAt) {
+            carFleetCount--;
+            stack.pop();
+           } 
+
+        }
+
+        return carFleetCount;
+    }
+}
