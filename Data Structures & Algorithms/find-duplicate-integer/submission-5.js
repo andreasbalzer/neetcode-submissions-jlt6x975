6f14df06cf26468a6ad68dc20fc2ba8caa64f8e1
Andreas Balzer <github@andreas-balzer.de>
2026-04-23 13:27:05 +0100

    Add: find-duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-5.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findDuplicate(nums) {
+        let slow = 0;
+        let fast = 0;
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow === fast) {
+                break;
+            }
+        }
+
+        fast = 0;
+        while (slow !== fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+
+        return fast;
+    }
+}
