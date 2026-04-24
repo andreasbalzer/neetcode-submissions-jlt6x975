7d96332db7a0d28d43e1b1270668cb0771a10f06
Andreas Balzer <github@andreas-balzer.de>
2026-04-24 15:41:36 +0100

    Add: reverse-nodes-in-k-group - submission-2

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-2.js
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @param {number} k
+     * @return {ListNode}
+     */
+    reverseKGroup(head, k) {
+        let length = 0;
+        let node = head;
+        while (node) {
+            length++
+            node = node.next;
+        }
+
+        const repeats = Math.floor(length / k);
+        const remainder = length % k;
+
+        node = new ListNode(undefined, head);
+        let overallHead = undefined;
+        for (let time = 0; time < repeats; time++) {
+            const [newHead, endOfReversed, rest] = this.reverse(node.next, k);
+            node.next = newHead;
+            node = endOfReversed;
+            endOfReversed.next = rest;
+            if (!overallHead) {
+                overallHead = newHead;
+            }
+        }
+
+        return overallHead;
+    }
+
+    /**
+     * Reverts a list for count nodes.
+     * @returns [new head after reverse, end after reverse, node after reversal of old list]
+     */
+    reverse(start, count) {
+        let prev = null;
+        let node = start;
+        let tmp = node.next;
+        while (node && count > 0) {
+            tmp = node.next;
+            node.next = prev;
+            prev = node;
+            node = tmp;
+            count--;
+        }
+
+        return [prev, start, tmp];
+    }
+}
