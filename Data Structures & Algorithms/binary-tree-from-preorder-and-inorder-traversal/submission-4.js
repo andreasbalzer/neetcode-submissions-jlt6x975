8d4603a246601c6df60a32bc2404edf58d2068ba
Andreas Balzer <github@andreas-balzer.de>
2026-04-27 12:16:58 +0100

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-4

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-4.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {number[]} preorder
+     * @param {number[]} inorder
+     * @return {TreeNode}
+     */
+    buildTree(preorder, inorder) {
+        const map = new Map();
+        inorder.forEach((value, index) => map.set(value, index));
+
+        if (preorder.length === 0 || inorder.length === 0) {
+            return null;
+        }
+
+        const newNode = new TreeNode(preorder[0]);
+        // preorder [1,2,3,4]
+        // inorder  [2,1,3,4]
+        const mid = map.get(preorder[0]);
+        newNode.left = this.buildTree(
+            preorder.slice(1, mid + 1),
+            inorder.slice(0, mid)
+        );
+        newNode.right = this.buildTree(
+            preorder.slice(mid + 1),
+            inorder.slice(mid + 1)
+        );
+        return newNode;
+    }
+}
