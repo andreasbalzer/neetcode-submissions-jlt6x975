2a7cd3073e8ca39017aa7a4d33e6779ed7d2e0c8
Andreas Balzer <github@andreas-balzer.de>
2026-05-04 14:54:52 +0100

    Add: design-word-search-data-structure - submission-5

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-5.js
@@ -0,0 +1,68 @@
+class Node {
+    constructor() {
+        this.children = new Map();
+        this.endOfWord = false;
+    }
+}
+class WordDictionary {
+    constructor() {
+        this.tree = new Node();
+    }
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    addWord(word) {
+        let node = this.tree;
+        for (let char of word) {
+            if (!node.children.has(char)) {
+                node.children.set(char, new Node());
+            }
+            node = node.children.get(char);
+        }
+
+        node.endOfWord = true;
+    }
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word) {
+        const helper = (index, node) => {
+            if (!node) {
+                return false;
+            }
+
+            if (index === word.length) {
+                return node.endOfWord;
+            }
+
+            while (index < word.length) {
+                if (!node) {
+                    return false;
+                }
+                
+                if (word[index] === ".") {
+                    let sub = false;
+                    for (let value of node.children.values()) {
+                        sub = sub || helper(index + 1, value);
+                    }
+                    return sub;
+                }
+
+                if (!node.children.has(word[index])) {
+                    return false;
+                }
+                
+                node = node.children.get(word[index]);
+                
+                index++;
+            }
+
+            return node.endOfWord;
+        };
+        return helper(0, this.tree);
+    }
+}
