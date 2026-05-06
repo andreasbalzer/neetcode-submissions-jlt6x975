f542aafd4760c5e7ddfca8c8da1d9a1936ed679f
Andreas Balzer <github@andreas-balzer.de>
2026-05-06 17:02:28 +0100

    Add: pacific-atlantic-water-flow - submission-3

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-3.js
@@ -0,0 +1,46 @@
+class Solution {
+    /**
+     * @param {number[][]} heights
+     * @return {number[][]}
+     */
+    pacificAtlantic(heights) {
+        const pacific = new Set();
+        const atlantic = new Set();
+        const dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+        const dfs = (y, x, set) => {
+            set.add(`${y}-${x}`);
+            for (let dir of dirs) {
+                if (y + dir[0] < 0 || y  + dir[0] >= heights.length ||
+                    x + dir[1] < 0 || x + dir[1] >= heights[0].length ||
+                    set.has(`${y + dir[0]}-${x + dir[1]}`) ||
+                    heights[y + dir[0]][x + dir[1]] < heights[y][x]) {
+                    continue;
+                }
+
+                dfs(y + dir[0], x + dir[1], set);
+            }
+        };
+        for (let x = 0; x < heights[0].length; x++) {
+            dfs(0, x, pacific);
+        }
+        for (let y = 0; y < heights.length; y++) {
+            dfs(y, 0, pacific);
+        }
+
+        for (let x = 0; x < heights[0].length; x++) {
+            dfs(heights.length - 1, x, atlantic);
+        }
+        for (let y = 0; y < heights.length; y++) {
+            dfs(y, heights[0].length - 1, atlantic);
+        }
+
+        const result = [];
+        for (let key of pacific.values()) {
+            if (atlantic.has(key)) {
+                result.push(key.split("-").map((item) => parseInt(item, 10)));
+            }
+        }
+ 
+        return result;
+    }
+}
