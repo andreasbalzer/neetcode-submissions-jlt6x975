80d4ad4d3d392b5c07230748fa91dee2ce1270a7
Andreas Balzer <github@andreas-balzer.de>
2026-05-07 00:01:02 +0100

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.js
@@ -0,0 +1,51 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {boolean}
+     */
+    validTree(n, edges) {
+        const parent = new Array(n + 1).fill(0).map((value, index) => index);
+        const rank = new Array(n + 1).fill(1);
+        let components = n;
+        const find = (node) => {
+            if (parent[node] !== node) {
+                parent[node] = find(parent[node]);
+            }
+
+            return parent[node];
+        };
+
+        const union = (node1, node2) => {
+            const p1 = find(node1);
+            const p2 = find(node2);
+
+            if (p1 === p2) {
+                return true;
+            }
+
+
+            components--;
+            if (rank[p1] > rank[p2]) {
+                rank[p1] += rank[p2];
+                parent[p2] = p1;
+            }
+            else {
+                rank[p2] += rank[p1];
+                parent[p1] = parent[p2];
+            }
+
+            return false;
+        };
+
+        for (let [node1, node2] of edges) {
+            if (union(node1, node2)) {
+                console.log("connected");
+                return false;
+            }
+        }
+
+        console.log(components);
+        return components === 1;
+    }
+}
