e06b8b0de58a39533c56eaf77638bdcbb53e586a
Andreas Balzer <github@andreas-balzer.de>
2026-05-08 15:05:21 +0100

    Add: count-connected-components - submission-2

---
+++ Data Structures & Algorithms/count-connected-components/submission-2.js
@@ -0,0 +1,49 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {number}
+     */
+    countComponents(n, edges) {
+        const rank = new Array(n + 1).fill(1);
+        const parent = new Array(n + 1).fill(0).map((_, index) => index);
+        const find = (node) => {
+            while (node !== parent[node]) {
+                parent[node] = parent[parent[node]];
+                node = parent[node];
+            }
+
+            return node;
+        };
+
+        const union = (node1, node2) => {
+            const parent1 = find(node1);
+            const parent2 = find(node2);
+
+            if (parent1 === parent2) {
+                return true;
+            }
+
+            if (rank[parent1] > rank[parent2]) {
+                rank[parent1] += rank[parent2];
+                parent[parent2] = parent1; 
+            }
+            else {
+                rank[parent2] += rank[parent1];
+                parent[parent1] = parent2;
+            }
+
+            return false;
+        };
+
+        for (let [node1, node2] of edges) {
+           if (union(node1, node2)) {
+           } 
+           else {
+            n--;
+           }
+        }
+
+        return n;
+    }
+}
