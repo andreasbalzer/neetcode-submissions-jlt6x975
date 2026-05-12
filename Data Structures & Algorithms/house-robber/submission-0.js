939e8e80cfac705dcb7e9f71a443c419bea5f882
Andreas Balzer <github@andreas-balzer.de>
2026-05-12 23:33:34 +0100

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+        let h1 = 0;
+        let h2 = 0;
+        let current = 0;
+        for (let house = nums.length - 1; house > 0; house--) {
+            current = Math.max(nums[house] + h2, h1);
+            console.log(house + " : " + current + " h1 " + h1 + " h2 " + h2);
+            h2 = h1;
+            h1 = current; 
+        }
+        current = Math.max(nums[0] + h2, h1);
+        console.log(current);
+        return Math.max(current, h1);
+        /**
+         * [ 1, 1, 3, 3] 0 0
+         * winning = Math.max(nums[index] + nums[index + 2], nums[index + 1] + nums[index + 3]) 
+         * 
+         * 
+         * 
+         * [2,9,  8,  3,6] 0, 0
+         *       14   6 6
+         *    15
+         * 16 
+         * 
+         */
+    }
+}
